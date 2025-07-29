f5773ac603c5a8cbad0eb1ad47bbe3b52b029e93
alvaro-max-git <alvaro.master@gmail.com>
2025-07-29 14:51:01 +0200

    Clases 17-18-19

--- springboot-web/src/main/java/com/alvaromax/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/alvaromax/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alvaromax.curso.springboot.webapp.springboot_web.models.User;
+import com.alvaromax.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -15,11 +16,24 @@ import java.util.Map;
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String,Object> detailsrest () {
+    public UserDto details () {
+        
+        UserDto userDto = new UserDto();
+        User user = new User ("Álvaro", "Muñoz");
+        
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+
+        return userDto;
+    }
+
+
+    @GetMapping("/details-map")
+    public Map<String,Object> detailsMap () {
 
         User user = new User ("Álvaro", "Muñoz");
-        Map<String, Object> body = new HashMap<>();
 
+        Map<String, Object> body = new HashMap<>();
         body.put("user", user);
         body.put("date", (new Date()).toString());
 
